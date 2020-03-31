2f67dac2c100d935a1d861f23cc7864c87261e8b
JDVR25 <jd.villamil@uniandes.edu.co>
2020-03-30 22:57:00 -0500

    1 binaries uploaded

--- scripts/dataCourses.js
+++
@@ -1,7 +0,0 @@
-import { Course } from './course.js';
-export var dataCourses = [
-    new Course("Desarrollo de Sw", "Cesar", 3),
-    new Course("Aleman 1", "Natalia", 3),
-    new Course("Sistemas Transaccionales", "Claudia", 3),
-    new Course("Ingles 8", "Jacob", 2)
-];
